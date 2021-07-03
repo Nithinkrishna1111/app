03aeb0197d022d5413d3b66e36f686e13f0f9f46
RamTadi <rtadi@uh.edu>
2021-07-02 22:11:30 -0500

    Ram - V2

--- src/components/Login/PriceQuote/PricequoteForm.js
+++ src/components/Login/PriceQuote/PricequoteForm.js
@@ -8,7 +8,6 @@ import {BrowserRouter as Router} from "react-router-dom";
 const PricequoteForm=( {onSubmit} )=> {
     const[refresh,errors,gallons,Delivery_Address,date,price,gallonChangeHandler,AddressChangeHandler,dateChangeHandler,priceChangeHandler]=PricequoteHandle(onSubmit,validation)
     const qprice=5
-
     return (
         <Router>
             <form >
@@ -16,7 +15,9 @@ const PricequoteForm=( {onSubmit} )=> {
 
                     &nbsp;&nbsp;&nbsp;
                     <div className='LoginForm-Input'>
+
                         <h1>Price Quote Form</h1>
+                        <label>Gallons Requested</label>
                         <input
                             name='Gallons Requested'
                             placeholder='Enter Gallons '
@@ -28,7 +29,7 @@ const PricequoteForm=( {onSubmit} )=> {
                     </div>
                     &nbsp;
                     <div className='LoginForm-Input'>
-                        {/*<label>Password</label>*/}
+                        <label>Delivery Address</label>
                         <input readOnly
                                name='Delivery Address'
                                placeholder='Delivery Address:'
@@ -40,17 +41,20 @@ const PricequoteForm=( {onSubmit} )=> {
                     &nbsp;
                     <div className='LoginForm-Input'>
                         {/*<label>Password</label>*/}
+                        <label>Date</label>
                         <input
+
                             name=''
                             placeholder='Pick the Delivery Date:'
                             type="date"
                             value={date}
                             onChange={dateChangeHandler}/>
+
                         {errors.Delivery_date && <p>*{errors.Delivery_date}</p>}
                     </div>
                     &nbsp;
                     <div className='LoginForm-Input'>
-                        {/*<label>Password</label>*/}
+                        <label>Suggested Price</label>
                         <input readOnly
                                name='Suggestedprice'
                                value={qprice}/>
@@ -59,7 +63,7 @@ const PricequoteForm=( {onSubmit} )=> {
 
                     &nbsp;
                     <div className='LoginForm-Input'>
-                        {/*<label>Password</label>*/}
+                        <label>Total Amount Due</label>
                         <input readOnly value={5*qprice}placeholder='Quote' />
 
                     </div>
